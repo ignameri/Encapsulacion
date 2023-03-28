3aed56dd8876106589148c5fb8855ac9d29129ce
Igna Meritano <122479080+ignameri@users.noreply.github.com>
2023-03-28 18:58:09 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Persona persona = new Persona();
+        persona.setNombre("Ignacio");
+        persona.setEdad(23);
+        persona.setTelefono(351350343);
+
+
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getTelefono());
+    }
+}
+
+class Persona {
+
+    private String nombre;
+    private int edad;
+    private int telefono;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre()
+    {
+        return this.nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public int getEdad()
+    {
+        return this.edad;
+    }
+
+    public void setTelefono (int telefono)
+    {
+        this.telefono = telefono;
+    }
+    public int getTelefono()
+    {
+        return this.telefono;
+    }
+}
